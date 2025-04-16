6e8bfaca112aa6ca751e545ec5b406c240219e31
sridurgas10 <76196643+sridurgas10@users.noreply.github.com>
2025-04-16 18:09:50 +0530

    Update cls2.py

--- cls2.py
+++ cls2.py
@@ -5,14 +5,12 @@ class Rectangle:
 
        
     def area(self):
-        #Area=Length*width
-       # print(f"area of rectangle is {self.length}*{self.width}")
+   
         return self.length*self.width
 
     def perimeter(self):
         return 2 *(self.length+self.width)
        
-        #print(f"perimeter of the rectangle is {self.length}{self.width}")
         
 rectangle=Rectangle(2,4) 
 print("Area " ,rectangle.area())
